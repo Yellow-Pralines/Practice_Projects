e27127c1f281bfaf9a02024d237076590e4a955e
Yellow-Pralines <107819552+Yellow-Pralines@users.noreply.github.com>
2022-06-20 08:18:57 -0700

    Create Shipping.py
    Sal runs the biggest shipping company in the tri-county area, Sal’s Shippers. Sal wants to make sure that every single one of his customers has the best, and most affordable experience shipping their packages.
    
    In this project, you’ll build a program that will take the weight of a package and determine the cheapest way to ship that package using Sal’s Shippers.
    Sal’s Shippers has several different options for a customer to ship their package:
    
    Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
    Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
    Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.

---
+++ Shipping.py
@@ -0,0 +1,45 @@
+name="John"
+weight= 8.4
+print("Your package's weight is " + str(weight) + "lb")
+ground_shipping=""
+premium_ground_shipping= 125
+drone_shipping=""
+
+#Cost of Ground Shipping
+if weight<=2:
+  ground_shipping=20+1.50*weight
+elif weight <=6:
+  ground_shipping=20+3.00*weight
+elif weight <=10:
+  ground_shipping=20+4.00*weight
+else:
+  ground_shipping=20+4.75*weight
+print("Cost using ground shipping: " + "$" + str(ground_shipping))
+
+#Cost of Premium Ground Shipping
+
+print("Cost using premium ground shipping: " + 
+"$" + str(premium_ground_shipping))
+
+#Cost of Drone Shipping
+if weight<=2:
+  drone_shipping=4.50*weight
+elif weight <=6:
+  drone_shipping=9.00*weight
+elif weight <=10:
+  drone_shipping=12.00*weight
+else:
+  drone_shipping=14.25*weight
+print("Cost using drone shipping: " + "$" + str(drone_shipping))
+
+cheapest=""
+minimum=min(ground_shipping, premium_ground_shipping, drone_shipping)
+if minimum==ground_shipping:
+  cheapest= "ground shipping"
+elif minimum==premium_ground_shipping:
+  cheapest= "premium ground shipping"
+else:
+  cheapest= "drone shipping"  
+
+print(name + "," + " the cheapest shipping option for your package is " + cheapest 
++ ".")
